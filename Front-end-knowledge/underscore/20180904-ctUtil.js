63bf5512c9532b2855e771a7e26de9d34704b7b6
careteenL <15074806497@163.com>
2018-09-07 17:05:17 +0800

    Update: add ctutil throttle

--- Front-end-knowledge/underscore/20180904-ctUtil.js
+++ Front-end-knowledge/underscore/20180904-ctUtil.js
@@ -43,12 +43,12 @@
    *         1.文本输入的验证（连续输入文字进行ajax验证，验证一次就行）
    *         2.每次添加元素自动触发排序函数（连续添加100个，排序函数会触发100次，理想情况只触发一次，即添加完所有内容以后触发）
    * @example
-   *    1.不传参数
+   *    // 1.不传参数
    *    function print () {
    *      console.log('hi careteen')
    *    }
    *    window.onscroll = _.debounce(print, 1000);
-   *    2.传参数
+   *    // 2.传参数
    *    function print (param) {
    *      console.log('hi ' + param)
    *    }
@@ -87,11 +87,60 @@
    * @desc 函数节流
    * @param {Function} func 
    * @param {Number} wait 
-   * @scence 
-   *         1.
+   * @param {Object} options:{leading: boolean, trailng: boolean}
+   *                          leading: 是否马上触发回调函数
+   *                          trailing: 是否触发最后一次回调函数
+   * @scence 固定时间段间断执行函数
+   *         1.DOM元素的拖拽功能（mousemove）
+   *         2.canvas模拟画板功能（mousemove）
+   *         3.联想搜索（keyup）
+   *         4.射击游戏的mousemove/keydown事件（单位时间只能发一颗子弹）
+   *         ...
    * @example
    */ 
-  _.throttle = function (func, wait) {
-
+  _.throttle = function (func, wait, options) {
+    var context, args, result;
+    var timer = null;
+    var previous = 0;
+    if (!options) {
+      options = {}
+    }
+    var later = function () {
+      // 如果设置第一个回调不立即执行
+      // 则每次触发回调后 previous 置为0
+      // 否则为当前时间戳
+      previous = options.leading === false ? 0 : _.now();
+      timer = null;
+      result = func.apply(context, args);
+      if (!timer) {
+        context = args = null;
+      }
+    }
+    return function () {
+      context = this;
+      args = arguments;
+      var now = _.now();
+      // 第一次执行回调 并且设置第一个回调不立即执行
+      if (!previous && options.leading === false) {
+        previous = now;
+      }
+      // 距离下次触发 func 还需要等待的时间
+      var remaining = wait - (now - previous);
+      // 间隔时间到了触发 func 或者 remaining > wait 表示客户端系统时间被修改过
+      if (remaining <= 0 || remaining > wait) {
+        if (timer) {
+          clearTimeout(timer);
+          timer = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timer) {
+          context = args = null;
+        }
+      } else if (!timer && options.trailing !== false) { // 最后一次需要触发的情况
+        timer = setTimeout(later, remaining);
+      }
+      return result;
+    }
   }
 }).call(this)
